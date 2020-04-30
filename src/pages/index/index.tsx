045901ef3c2256236f9bfa8e070c54bb986ff8d0
刘婵 <liuchan@feewee.cn>
2020-04-30 17:46:27 +0800

    first commoit

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -12,7 +12,7 @@ export default class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: 'demo'
   }
 
   componentWillMount () { }
@@ -28,7 +28,7 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Text>第一次提交!</Text>
       </View>
     )
   }
